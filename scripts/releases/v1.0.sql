810a48e271a6c12a98f60f99e3ca4b5c05fadee1
wondi w <wondts@gmail.com>
2025-03-25 08:30:46 -0500

    Ensure v1.0.sql is idempotent by adding existence chck for test object

--- scripts/releases/v1.0.sql
+++ scripts/releases/v1.0.sql
@@ -1,1 +1,7 @@
-CREATE TABLE Test (Id INT)
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[demo]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE demo (
+        ID INT PRIMARY KEY,
+        Name NVARCHAR(50)
+    );
+END
